0ea3f81a362f4afb68201724d67469b6fd4f8a69
Maximilian Schmidt <maximilian@schmidt.so>
2025-02-16 12:45:15 +0100

    refactor some stuff

--- tools/machine-learning/joint-prediction/scripts/linear_regression.py
+++ tools/machine-learning/joint-prediction/scripts/linear_regression.py
@@ -1,46 +1,58 @@
 import datetime
+from collections.abc import Callable, Iterable
+from typing import Self
 
 import click
-import cvxpy as cp
 import numpy as np
 import plotly.graph_objects as go
 import polars as pl
 import polars.selectors as ps
+from numpy.typing import NDArray
 from plotly.subplots import make_subplots
+from polars._typing import ColumnNameOrSelector
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.ensemble import (
     HistGradientBoostingRegressor,
 )
-from sklearn.linear_model import Ridge
 
 
 class LastSensordataRegressor(BaseEstimator, RegressorMixin):
-    def __init__(self, index_of_last_sensor_data: int = 0):
+    index_of_last_sensor_data: int
+
+    def __init__(self, index_of_last_sensor_data: int = 0) -> None:
         self.index_of_last_sensor_data = index_of_last_sensor_data
 
-    def fit(self, X, y):
+    def fit(
+        self,
+        x: NDArray[np.float64] | pl.DataFrame,
+        y: NDArray[np.float64] | pl.DataFrame,
+    ) -> Self:
+        _ = x, y
         return self
 
-    def predict(self, X):
-        if isinstance(X, pl.DataFrame):
-            X = X.to_numpy()
-        return X[:, self.index_of_last_sensor_data]
+    def predict(
+        self,
+        x: NDArray[np.float64] | pl.DataFrame,
+    ) -> NDArray[np.float64]:
+        if isinstance(x, pl.DataFrame):
+            x = x.to_numpy()
+        return x[:, self.index_of_last_sensor_data]
 
 
-def fit_constrained_ridge(X_train, y_train, alpha: float = 1.0) -> Ridge:
-    model = Ridge(fit_intercept=False)
-    n_features = X_train.shape[1]
-
-    w = cp.Variable(n_features)
-    objective = cp.Minimize(
-        cp.sum_squares(X_train @ w - y_train) + alpha * cp.sum_squares(w),
-    )
-    constraints = [cp.sum(w) == 1]
-    problem = cp.Problem(objective, constraints)
-    problem.solve()
-    model.coef_ = w.value
-    model.intercept_ = 0.0
-    return model
+# def fit_constrained_ridge(X_train, y_train, alpha: float = 1.0) -> Ridge:
+#     model = Ridge(fit_intercept=False)
+#     n_features = X_train.shape[1]
+#
+#     w = cp.Variable(n_features)
+#     objective = cp.Minimize(
+#         cp.sum_squares(X_train @ w - y_train) + alpha * cp.sum_squares(w),
+#     )
+#     constraints = [cp.sum(w) == 1]
+#     problem = cp.Problem(objective, constraints)
+#     problem.solve()
+#     model.coef_ = w.value
+#     model.intercept_ = 0.0
+#     return model
 
 
 def xyz_name(index: int) -> str:
@@ -51,28 +63,42 @@ def xyz_name(index: int) -> str:
     }[index]
 
 
-def unnest_structs(df: pl.DataFrame, name: str, depth: int) -> pl.DataFrame:
-    column = df.select(name)
-    for i in range(depth):
-        struct_cols = column.columns
-        new_columns = []
+def unnest_structs(
+    df: pl.DataFrame,
+    exprs: ColumnNameOrSelector | Iterable[ColumnNameOrSelector],
+    *,
+    depth: int = 1,
+    list_to_struct_fields: list[str] | Callable[[int], str] = xyz_name,
+) -> pl.DataFrame:
+    if depth < 1:
+        return df
 
-        for col in struct_cols:
-            values = column[col]
-            if isinstance(values.dtype, pl.List):
-                values = values.list.to_struct(fields=xyz_name)
+    nested_df = df.select(exprs)
 
-            unnested = values.struct.unnest()
-            renamed = unnested.rename(
-                {sub_col: f"{col}.{sub_col}" for sub_col in unnested.columns},
-            )
-            new_columns.append(renamed)
-        column = pl.concat(
-            [column.drop(struct_cols), *new_columns],
+    for _ in range(depth):
+        current_cols = nested_df.columns
+        unnested_columns: list[pl.DataFrame] = []
+
+        for current_col in current_cols:
+            series = nested_df[current_col]
+            if isinstance(series.dtype, pl.List):
+                series = series.list.to_struct(fields=list_to_struct_fields)
+
+            unnested = series.struct.unnest()
+
+            rename_map = {
+                subcol: f"{current_col}.{subcol}"
+                for subcol in unnested.columns
+            }
+            renamed = unnested.rename(rename_map)
+            unnested_columns.append(renamed)
+
+        nested_df = pl.concat(
+            [nested_df.drop(current_cols), *unnested_columns],
             how="horizontal",
         )
 
-    return pl.concat([df.drop(name), column], how="horizontal")
+    return pl.concat([df.drop(exprs), nested_df], how="horizontal")
 
 
 def train_test_split(
@@ -102,7 +128,7 @@ def shift_expr(feature: str, k: int, group: str) -> pl.Expr:
 
 
 def generate_fit_data(
-    X: pl.DataFrame,
+    x: pl.DataFrame,
     y: pl.DataFrame,
     k_history: int,
     k_horizon: int,
@@ -110,7 +136,7 @@ def generate_fit_data(
 ) -> tuple[pl.DataFrame, pl.DataFrame]:
     past_features = [
         shift_expr(feature, k, group_key)
-        for feature in X.columns
+        for feature in x.columns
         if feature != group_key
         for k in range(k_history + 1)
     ]
@@ -120,15 +146,15 @@ def generate_fit_data(
         for target in y.columns
         if target != group_key
     ]
-    X = X.select(past_features)
+    x = x.select(past_features)
     y = y.select(future_features)
 
     is_not_null = (
-        X.select(pl.all_horizontal(pl.all().is_not_null())).to_series()
+        x.select(pl.all_horizontal(pl.all().is_not_null())).to_series()
         & y.select(pl.all_horizontal(pl.all().is_not_null())).to_series()
     )
 
-    return X.filter(is_not_null), y.filter(is_not_null)
+    return x.filter(is_not_null), y.filter(is_not_null)
 
 
 def filter_walk(df: pl.DataFrame) -> pl.DataFrame:
@@ -145,49 +171,48 @@ def filter_walk(df: pl.DataFrame) -> pl.DataFrame:
 
 
 def improve_over_baseline(
-    X_train: pl.DataFrame,
+    x_train: pl.DataFrame,
     y_train: pl.DataFrame,
-    X_test: pl.DataFrame,
+    x_test: pl.DataFrame,
     y_test: pl.DataFrame,
 ) -> float:
     baseline = LastSensordataRegressor()
-    baseline_prediction = baseline.fit(X_train, y_train).predict(X_test)
+    baseline_prediction = baseline.fit(x_train, y_train).predict(x_test)
     baseline_error = np.square(
         baseline_prediction - y_test.to_numpy().flatten(),
     )
-    # model = fit_constrained_ridge(X_train.to_numpy(), y_train.to_numpy().flatten())
-    # model = Ridge()
     model = HistGradientBoostingRegressor("absolute_error")
-    prediction = model.fit(X_train, y_train.to_series()).predict(X_test)
+    model.fit(x_train, y_train.to_series())
+    prediction = model.predict(x_test)
     model_error = np.square(prediction - y_test.to_numpy().flatten())
 
     return np.rad2deg(np.mean(model_error - baseline_error))
 
 
 def fit_model(
-    X_train: pl.DataFrame,
+    x_train: pl.DataFrame,
     y_train: pl.DataFrame,
-    X_test: pl.DataFrame,
+    x_test: pl.DataFrame,
     y_test: pl.DataFrame,
     k_history: int,
     k_horizon: int,
     group_key: str,
 ) -> float:
-    X_train, y_train = generate_fit_data(
-        X_train,
+    x_train, y_train = generate_fit_data(
+        x_train,
         y_train,
         k_history,
         k_horizon,
         group_key,
     )
-    X_test, y_test = generate_fit_data(
-        X_test,
+    x_test, y_test = generate_fit_data(
+        x_test,
         y_test,
         k_history,
         k_horizon,
         group_key,
     )
-    return improve_over_baseline(X_train, y_train, X_test, y_test)
+    return improve_over_baseline(x_train, y_train, x_test, y_test)
 
 
 @click.command()
@@ -195,13 +220,17 @@ def fit_model(
     "parquet",
     type=click.Path(exists=True, dir_okay=False, readable=True),
 )
-def main(parquet: str):
-    df = pl.read_parquet(parquet)
-    df = filter_walk(df)
+def main(parquet: str) -> None:
+    data = pl.read_parquet(parquet)
+    data = filter_walk(data)
 
-    df = unnest_structs(df, "motor_commands.positions", 2)
-    df = unnest_structs(df, "sensor_data.positions", 2)
-    df = unnest_structs(df, "sensor_data.inertial_measurement_unit", 2)
+    data = unnest_structs(data, "motor_commands.positions", depth=2)
+    data = unnest_structs(data, "sensor_data.positions", depth=2)
+    data = unnest_structs(
+        data,
+        "sensor_data.inertial_measurement_unit",
+        depth=2,
+    )
 
     features = [
         "chunk_id",
@@ -224,13 +253,13 @@ def main(parquet: str):
         "sensor_data.inertial_measurement_unit.angular_velocity.y",
     ]
 
-    train_df, test_df = train_test_split(df, "chunk_id", test_fraction=0.2)
-    X_train, y_train = xy_split(train_df, features, targets)
-    X_test, y_test = xy_split(test_df, features, targets)
+    train_df, test_df = train_test_split(data, "chunk_id", test_fraction=0.2)
+    x_train, y_train = xy_split(train_df, features, targets)
+    x_test, y_test = xy_split(test_df, features, targets)
 
-    k_history_values = np.arange(0, 50, 1)
-    k_horizon_values = np.arange(0, 22)
-    scores = np.zeros((k_history_values.size, k_horizon_values.size))
+    # k_history_values = np.arange(0, 50, 1)
+    # k_horizon_values = np.arange(0, 22)
+    # scores = np.zeros((k_history_values.size, k_horizon_values.size))
 
     # with tqdm(total=k_history_values.size * k_horizon_values.size) as pbar:
     #     for (i, k_history), (j, k_horizon) in product(
@@ -254,7 +283,7 @@ def main(parquet: str):
     # )
 
     k_history = 25
-    scores = np.zeros_like(k_horizon_values, dtype=np.float64)
+    # scores = np.zeros_like(k_horizon_values, dtype=np.float64)
     # for j, k_horizon in enumerate(tqdm(k_horizon_values)):
     #     scores[j] = fit_model(
     #         X_train,
@@ -265,21 +294,21 @@ def main(parquet: str):
     #         k_horizon,
     #         "chunk_id",
     #     )
-    fig = go.Figure()
-    fig.add_bar(x=k_horizon_values, y=scores)
-    fig.show()
+    # fig = go.Figure()
+    # fig.add_bar(x=k_horizon_values, y=scores)
+    # fig.show()
 
     k_history = 25
     k_horizon = 2
-    X_train, y_train = generate_fit_data(
-        X_train,
+    x_train, y_train = generate_fit_data(
+        x_train,
         y_train,
         k_history,
         k_horizon,
         "chunk_id",
     )
-    X_test, y_test = generate_fit_data(
-        X_test,
+    x_test, y_test = generate_fit_data(
+        x_test,
         y_test,
         k_history,
         k_horizon,
@@ -287,11 +316,11 @@ def main(parquet: str):
     )
     model = HistGradientBoostingRegressor()
     # model = RandomForestRegressor()
-    model.fit(X_train, y_train.to_series())
-    prediction = model.predict(X_test)
+    model.fit(x_train, y_train.to_series())
+    prediction = model.predict(x_test)
     # print(model.coef_.tolist(), model.intercept_)
 
-    baseline = LastSensordataRegressor().fit(X_train, y_train).predict(X_test)
+    baseline = LastSensordataRegressor().fit(x_train, y_train).predict(x_test)
 
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True)
     fig.add_scatter(
@@ -320,10 +349,10 @@ def main(parquet: str):
 
 
 def plot_error_matrix(
-    matrix,
-    k_history_values,
-    k_horizon_values,
-):
+    matrix: NDArray[np.float64],
+    k_history_values: NDArray[np.int64],
+    k_horizon_values: NDArray[np.int64],
+) -> None:
     fig = go.Figure()
     fig.add_contour(
         z=matrix,
@@ -340,7 +369,9 @@ def plot_error_matrix(
     )
 
     fig.show()
-    fig.write_html(f"images/{datetime.datetime.now()}_heatmap.html")
+    fig.write_html(
+        f"images/{datetime.datetime.now(tz=datetime.UTC)}_heatmap.html",
+    )
 
 
 if __name__ == "__main__":
