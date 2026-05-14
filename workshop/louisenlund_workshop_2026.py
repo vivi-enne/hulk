ae0c3c79a8abbf7d89715521c89bcff2d21e5e53
vivi-enne <vivienne.schwabe@tuhh.de>
2026-05-14 19:48:14 +0200

    add shoulder + elbow angle visualization

--- workshop/louisenlund_workshop_2026.py
+++ workshop/louisenlund_workshop_2026.py
@@ -9,12 +9,29 @@ def _():
     import marimo as mo
     import workshop
 
-    return mo, workshop
+    from pathlib import Path
+    import os
+    import marimo as mo
+    from mujoco import MjData, MjModel, Renderer, mj_step, mj_resetData, mj_forward
+    from workshop import MujocoViewer
+
+    return (
+        MjData,
+        MjModel,
+        MujocoViewer,
+        Path,
+        Renderer,
+        mj_forward,
+        mj_resetData,
+        mo,
+        os,
+        workshop,
+    )
 
 
 @app.cell
 def _(mo, workshop):
-    mo.md(f"""
+    mo.md(rf"""
     Version {workshop.TEST}
     """)
     return
@@ -42,12 +59,7 @@ def _(mo):
 
 
 @app.cell
-def _(mo):
-    from pathlib import Path
-
-    from mujoco import MjData, MjModel, Renderer, mj_step, mj_resetData, mj_forward
-    from workshop import MujocoViewer
-    import os
+def _(MjData, MjModel, MujocoViewer, Path, Renderer, mj_forward, mo, os):
 
     os.environ["MUJOCO_GL"] = "glfw"
 
@@ -59,13 +71,73 @@ def _(mo):
     viewer = mo.ui.anywidget(MujocoViewer())
     interval = 0.1
 
-    def advance_simulation(
-        mj_model: MjModel,
-        mj_data: MjData,
-        dt: float,
-    ) -> None:
-        n_steps = int(dt / model.opt.timestep / 2)
-        mj_step(mj_model, mj_data, nstep=n_steps)
+
+
+    index_shoulder_roll = model.joint("Right_Shoulder_Roll").id
+    index_shoulder_pitch = model.joint("ARight_Shoulder_Pitch").id
+    index_elbow_yaw = model.joint("Right_Elbow_Yaw").id
+    index_elbow_pitch = model.joint("Right_Elbow_Pitch").id
+
+    ctrl_min_shoulder_roll = model.jnt_range[index_shoulder_roll, 0]
+    ctrl_max_shoulder_roll = model.jnt_range[index_shoulder_roll, 1]
+    ctrl_min_shoulder_pitch = model.jnt_range[index_shoulder_pitch, 0]
+    ctrl_max_shoulder_pitch = model.jnt_range[index_shoulder_pitch, 1]
+    ctrl_min_elbow_yaw = model.jnt_range[index_elbow_yaw, 0]
+    ctrl_max_elbow_yaw = model.jnt_range[index_elbow_yaw, 1]
+    ctrl_min_elbow_pitch = model.jnt_range[index_elbow_pitch, 0]
+    ctrl_max_elbow_pitch = model.jnt_range[index_elbow_pitch, 1]
+
+    # slider for joint movement
+    slider_shoulder_roll = mo.ui.slider(start=ctrl_min_shoulder_roll, stop=ctrl_max_shoulder_roll, label="Schulter roll", value=0, step= 0.1)
+    slider_shoulder_pitch = mo.ui.slider(start=ctrl_min_shoulder_pitch, stop=ctrl_max_shoulder_pitch, label="Schulter pitch", value=0, step=0.1)
+    slider_elbow_yaw = mo.ui.slider(start=ctrl_min_elbow_yaw, stop=ctrl_max_elbow_yaw, label="Ellbogen yaw", value=0, step=0.1)
+    slider_elbow_pitch = mo.ui.slider(start=ctrl_min_elbow_pitch, stop=ctrl_max_elbow_pitch, label="Ellbogen pitch", value=0, step=0.1)
+
+    mj_forward(model, data)
+    renderer.update_scene(data, camera="overview_cam")
+    initial_pixels = renderer.render()
+    viewer.update(initial_pixels)
+    return (
+        data,
+        index_elbow_pitch,
+        index_elbow_yaw,
+        index_shoulder_pitch,
+        index_shoulder_roll,
+        interval,
+        model,
+        renderer,
+        slider_elbow_pitch,
+        slider_elbow_yaw,
+        slider_shoulder_pitch,
+        slider_shoulder_roll,
+        viewer,
+    )
+
+
+@app.cell
+def _(
+    MjData,
+    MjModel,
+    data,
+    index_elbow_pitch,
+    index_elbow_yaw,
+    index_shoulder_pitch,
+    index_shoulder_roll,
+    interval,
+    mj_forward,
+    mj_resetData,
+    mo,
+    model,
+    renderer,
+    slider_elbow_pitch,
+    slider_elbow_yaw,
+    slider_shoulder_pitch,
+    slider_shoulder_roll,
+    viewer,
+):
+    def advance_simulation(mj_model: MjModel,mj_data: MjData, dt: float) -> None:
+        mj_forward(mj_model, mj_data)
+
 
     def update(_):
         advance_simulation(model, data, interval)
@@ -75,18 +147,32 @@ def _(mo):
 
     def reset_simulation(_):
         mj_resetData(model, data)
-        mj_forward(model, data)
 
-        renderer.update_scene(data, camera="overview_cam")
-        rendered_pixels = renderer.render()
-        viewer.update(rendered_pixels)
+
+    data.qpos[index_shoulder_roll] = slider_shoulder_roll.value
+    data.qpos[index_shoulder_pitch] = slider_shoulder_pitch.value 
+    data.qpos[index_elbow_yaw] = slider_elbow_yaw.value
+    data.qpos[index_elbow_pitch] = slider_elbow_pitch.value
+
+    mj_forward(model, data)
+
+    renderer.update_scene(data, camera="overview_cam")
+    rendered_pixels = renderer.render()
+    viewer.update(rendered_pixels)
 
     restart_btn = mo.ui.button(label="🔄 Simulation neustarten", on_change=reset_simulation)
     refresh_timer = mo.ui.refresh(default_interval=interval, on_change=update)
 
-    mo.vstack([mo.hstack([refresh_timer, restart_btn]), viewer])
-    return
-
+    mo.vstack(
+    [
+       mo.hstack(
+           [
+               mo.vstack([slider_shoulder_roll, slider_shoulder_pitch, slider_elbow_yaw, slider_elbow_pitch]),
+               refresh_timer, restart_btn,
+           ]
+       ),
+       viewer,
+    ])
     return
 
 
