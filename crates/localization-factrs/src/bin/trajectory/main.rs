29def7b87f759bd65a0ce5e76b054ebf26023005
okiwi6 <45100017+oleflb@users.noreply.github.com>
2026-05-23 13:32:05 +0200

    Parse simulation data

--- crates/localization-factrs/src/bin/trajectory/main.rs
+++ crates/localization-factrs/src/bin/trajectory/main.rs
@@ -1,9 +1,17 @@
 pub mod simulation_data;
 
-use std::path::PathBuf;
+use std::{
+    path::PathBuf,
+    time::{Duration, Instant, SystemTime},
+};
 
+use booster::ImuState;
 use clap::Parser;
 use color_eyre::{Result, eyre::Context};
+use indicatif::ProgressIterator;
+use linear_algebra::IntoFramed;
+use localization_factrs::{backend::BackendConfiguration, initialize};
+use nalgebra::{Matrix3, Vector3, vector};
 
 use crate::simulation_data::SimulationData;
 
@@ -17,7 +25,33 @@ pub fn main() -> Result<()> {
     let data = std::fs::read_to_string(arguments.simulation)?;
     let data: SimulationData = serde_json::from_str(&data).wrap_err("failed to deserialize")?;
 
-    dbg!(data);
+    let (mut frontend, mut backend) = initialize(BackendConfiguration {
+        knot_spacing: Duration::from_millis(200),
+        max_optimization_window: Duration::from_secs(3),
+        gyroscope_noise: Matrix3::identity() * 0.01,
+        accelerometer_noise: Matrix3::identity() * 0.01,
+        gravity: vector![0.0, 0.0, 9.81],
+    });
+    let now = SystemTime::now();
+
+    for frame in data.measurements.iter().progress() {
+        let angular_velocity = Vector3::from(frame.imu.angular_velocity)
+            .cast::<f32>()
+            .framed();
+        let linear_acceleration = Vector3::from(frame.imu.linear_acceleration)
+            .cast::<f32>()
+            .framed();
+
+        frontend.ingest_imu(
+            now + Duration::from_secs_f64(frame.timestamp_seconds),
+            ImuState {
+                roll_pitch_yaw: vector![0.0, 0.0, 0.0].framed(),
+                angular_velocity,
+                linear_acceleration,
+            },
+        )?;
+        backend.solve_once()?;
+    }
 
     Ok(())
 }
--- crates/localization-factrs/src/bin/trajectory/simulation_data.rs
+++ crates/localization-factrs/src/bin/trajectory/simulation_data.rs
@@ -2,51 +2,51 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SimulationData {
-    start_pose: SimulatorState,
-    landmark_global_positions: LandmarkGlobalPositions,
-    measurements: Vec<SimulatorMeasurement>,
+    pub start_pose: SimulatorState,
+    pub landmark_global_positions: LandmarkGlobalPositions,
+    pub measurements: Vec<SimulatorMeasurement>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LandmarkGlobalPositions {
-    corner_top_left: [f64; 3],
-    corner_top_right: [f64; 3],
-    corner_bottom_left: [f64; 3],
-    corner_bottom_right: [f64; 3],
+    pub corner_top_left: [f64; 3],
+    pub corner_top_right: [f64; 3],
+    pub corner_bottom_left: [f64; 3],
+    pub corner_bottom_right: [f64; 3],
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SimulatorMeasurement {
-    timestamp_seconds: f64,
-    imu: SimulatorImuMeasurement,
-    ground_truth_pose: SimulatorState,
-    visual_features: Option<SimulatorDetectedFeatures>,
+    pub timestamp_seconds: f64,
+    pub imu: SimulatorImuMeasurement,
+    pub ground_truth_pose: SimulatorState,
+    pub visual_features: Option<SimulatorDetectedFeatures>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SimulatorImuMeasurement {
-    angular_velocity: [f64; 3],
-    linear_acceleration: [f64; 3],
+    pub angular_velocity: [f64; 3],
+    pub linear_acceleration: [f64; 3],
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SimulatorState {
-    position: [f64; 3],
-    quaternion_wxyz: [f64; 4],
-    linear_velocity_global: [f64; 3],
-    angular_velocity_local: [f64; 3],
+    pub position: [f64; 3],
+    pub quaternion_wxyz: [f64; 4],
+    pub linear_velocity_global: [f64; 3],
+    pub angular_velocity_local: [f64; 3],
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SimulatorDetectedFeatures {
-    detections: Vec<[f64; 2]>,
-    intrinsics: CameraIntrinsics,
+    pub detections: Vec<[f64; 2]>,
+    pub intrinsics: CameraIntrinsics,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CameraIntrinsics {
-    focal_x: f64,
-    focal_y: f64,
-    center_x: f64,
-    center_y: f64,
+    pub focal_x: f64,
+    pub focal_y: f64,
+    pub center_x: f64,
+    pub center_y: f64,
 }
